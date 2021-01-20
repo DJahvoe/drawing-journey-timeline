ee9e62fd50ccd69fba1bab675d306bfc83bd52c6
Rito038 <kevinadrian038@gmail.com>
2021-01-21 00:14:52 +0700

    coba

--- test.js
+++ test.js
@@ -11,6 +11,12 @@ const artList = [
 			'https://cdnb.artstation.com/p/assets/images/images/029/639/673/large/rizky-suwoto-suisei1final.jpg?1598202597',
 		created_at: '19-01-2021 20:15',
 	},
+	{
+		name: 'Hoshimachi Suisei at Concert ABCD',
+		url:
+			'https://cdnb.artstation.com/p/assets/images/images/029/639/673/large/rizky-suwoto-suisei1final.jpg?1598202597',
+		created_at: '19-01-2021 20:15',
+	},
 ];
 
 export default artList;
