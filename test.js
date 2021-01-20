15716869bfff82a3614d76fab6fe2dfe4041f15c
satriaade <dnaspeedmania@gmail.com>
2021-01-21 00:06:22 +0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,16 @@
+const artList = [
+	{
+		name: 'Hoshimachi Suisei at Concert',
+		url:
+			'https://cdnb.artstation.com/p/assets/images/images/029/639/673/large/rizky-suwoto-suisei1final.jpg?1598202597',
+		created_at: '19-01-2021 20:15',
+	},
+	{
+		name: 'Hoshimachi Suisei at Concert ABCD',
+		url:
+			'https://cdnb.artstation.com/p/assets/images/images/029/639/673/large/rizky-suwoto-suisei1final.jpg?1598202597',
+		created_at: '19-01-2021 20:15',
+	},
+];
+
+export default artList;
